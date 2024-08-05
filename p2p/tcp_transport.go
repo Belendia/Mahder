e9ce06b01d7db8a606f7637c19a0e3773901d9fa
Belendia <belendia@gmail.com>
2024-08-05 20:45:52 +0300

    Add listen and accept logic to the tcp transport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -17,4 +18,34 @@ func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddr,
 	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() (Peer, error) {
+	for {
+
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting TCP connection: %v\n", err)
+			return nil, err
+		}
+
+		go t.handleConn(conn)
+	}
+
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("Accepted TCP connection from %v\n", conn.RemoteAddr())
 }
